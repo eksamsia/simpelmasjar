e5f665e1a37de7d29105ca957e5cee12518604ba
eksamsia <ekasamsiati03@mail.ugm.ac.id>
2022-06-27 13:35:29 +0700

    change bg

--- application/views/users/auth/login.php
+++ application/views/users/auth/login.php
@@ -67,7 +67,7 @@ $get_data = $this->db->get('kontak')->row();
 
     <div class="limiter">
         <!-- <div class="container-login100" style="background-image: url('assets/login/images/bg-01.jpg');"> -->
-        <div class="container-login100" style="background-image: url('../assets/login/images/bgmasjar.jpg');">
+        <div class="container-login100" style="background-image: url('../assets/login/images/bgmasjar1.jpg');">
             <div class="wrap-login100 p-t-30 p-b-50">
                 <span class="login100-form-title p-b-41">
                     <?php
