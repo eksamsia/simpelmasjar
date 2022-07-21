3b6db38c06cb57e0420f6ee7981244c9ff310f05
adiningtyas <adiningtyas81@gmail.com>
2022-07-21 11:22:24 +0700

    fix download surat

--- application/controllers/Word.php
+++ application/controllers/Word.php
@@ -6,43 +6,42 @@ use PhpOffice\PhpWord\Writer\Word2007;
 
 class Word extends CI_Controller {
 
-	public function index()
+
+	public function print_pengajuan()
 	{
-		// $phpWord = new PhpWord();
-		// $section = $phpWord->addSection();
-		// $section->addText('Hello World !');
-		
-		// $writer = new Word2007($phpWord);
-		
-		// $filename = 'simple';
-		
-		// header('Content-Type: application/msword');
-        // header('Content-Disposition: attachment;filename="'. $filename .'.docx"'); 
-		// header('Cache-Control: max-age=0');
-		
-		// $writer->save('php://output');
+        $get_data= $this->ambil_data($this->input->post('id_download'));
 
         $phpWord = new \PhpOffice\PhpWord\PhpWord();
         $template = $phpWord->loadTemplate('template.docx');
 
-        $template->setValue('nama', 'Eka Samsiati Putri');
-        $template->setValue('status', 'Single');
-        $template->setValue('alamat', 'Kudus');
+        $template->setValue('nama', help_nama_user($get_data->id_user));
+        $template->setValue('status', $get_data->status_pemohon);
+        $template->setValue('alamat', $get_data->alamat);
 
         $temp_filename = 'surat-kerja-praktek.docx';
         $template->saveAs($temp_filename);
+        echo json_encode($temp_filename);
 
-        header('Content-Description: File Transfer');
-        header('Content-Type: application/octet-stream');
-        header('Content-Disposition: attachment; filename='.$temp_filename);
-        header('Content-Transfer-Encoding: binary');
-        header('Expires: 0');
-        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-        header('Pragma: public');
-        header('Content-Length: ' . filesize($temp_filename));
-        flush();
-        readfile($temp_filename);
-        unlink($temp_filename);
-        exit;        
-	}
+        // header('Content-Description: File Transfer');
+        // header('Content-Type: application/octet-stream');
+        // header('Content-Disposition: attachment; filename='.$temp_filename);
+        // header('Content-Transfer-Encoding: binary');
+        // header('Expires: 0');
+        // header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+        // header('Pragma: public');
+        // header('Content-Length: ' . filesize($temp_filename));
+        // flush();
+        // readfile($temp_filename);
+        // unlink($temp_filename);
+        // exit;       
+    }
+
+    private function ambil_data($id){
+        $this->load->database();
+        $this->db->select('*');
+        $this->db->from('pengajuan');
+        $this->db->where('id', $id);
+        $q = $this->db->get()->row();
+        return $q;
+    }
 }
