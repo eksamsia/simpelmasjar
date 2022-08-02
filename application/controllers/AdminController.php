d7f234929d11321a6c6ff9dd26179f06f9ac9ed4
eksamsia <ekasamsiati03@mail.ugm.ac.id>
2022-08-02 14:07:29 +0700

    remove useless file and change the view home

--- application/controllers/AdminController.php
+++ application/controllers/AdminController.php
@@ -1,41 +1,43 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-include('application/controllers/auth/DefaultController.php');
+defined('BASEPATH') or exit('No direct script access allowed');
+include 'application/controllers/auth/DefaultController.php';
 
-class AdminController extends DefaultController {
+class AdminController extends DefaultController
+{
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function __construct()
-	{
-		parent::__construct();
-		$this->checkLogin();
-	}
+    /**
+     * Index Page for this controller.
+     *
+     * Maps to the following URL
+     *         http://example.com/index.php/welcome
+     *    - or -
+     *         http://example.com/index.php/welcome/index
+     *    - or -
+     * Since this controller is set as the default controller in
+     * config/routes.php, it's displayed at http://example.com/
+     *
+     * So any other public methods not prefixed with an underscore will
+     * map to /index.php/welcome/<method_name>
+     * @see https://codeigniter.com/user_guide/general/urls.html
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->checkLogin();
+    }
 
-	public function index()
-	{
-		$data['list_rr'] = $this->getData();
-		$this->load->view('users/page/home',$data);
-	}
+    public function index()
+    {
+        $data['data_pengajuan'] = $this->getData();
+        $this->load->view('users/page/pengajuan', $data);
+    }
 
-	private function getData(){
-		$this->load->database();
-		$this->db->select('*');
-		$this->db->order_by("id", "asc");
-		return $this->db->get('master_ruang')->result();
-	}
+    private function getData()
+    {
+        $this->load->database();
+        $this->db->select('*');
+        $this->db->order_by("id", "asc");
+        return $this->db->get('pengajuan')->result();
+    }
 
 }
